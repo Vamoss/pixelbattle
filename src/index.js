91ad8223cd52db4c6101bb894286b0d33c04d9cd
vamoss <vamoss@vamoss.com.br>
2018-03-17 19:39:42 -0300

    minimum zoom 15

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ var mapEl = document.createElement('div');
 mapEl.id = 'map';
 document.body.appendChild(mapEl);
 
-var map = L.map('map');
+var map = L.map('map', {
+	minZoom: 15,
+    maxZoom: 19
+});
 map.setView([-22.9707, -43.1823], 15);
 
 var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
+	maxZoom: map.getMaxZoom(),
 	attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
